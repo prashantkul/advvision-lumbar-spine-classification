e728bd0d0a6032e417d008f7b54c28248ad05f4a
Steve <Steve@pskulkarni.altostrat.com>
2024-08-11 06:58:01 +0000

    resnet update

--- vision_project/vision_models/resnetmodel.py
+++ vision_project/vision_models/resnetmodel.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from keras.models import Model
 from tqdm.keras import TqdmCallback
-from keras.applications import ResNet101
+from keras.applications.resnet101 import ResNet101
 from keras.optimizers import Adam
 
 class ResNetModelTrainer:
