6743d874f8115132c8dc618789407ff2ffdf8e31
oa-kwaku <60941495+oa-kwaku@users.noreply.github.com>
2024-08-15 23:09:26 -0400

    feat: stage2 dataset for severity (#21)

---
+++ vision_project/stage_2_dataset_test.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+import tensorflow as tf
+
+from vision_models.stage2.helper import tensorize
+from vision_models.stage2.severity_prediction_dataset import SeverityPredictionDataset
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Suppress INFO and WARNING logs
+
+ground_truth_path = 'vision_models/stage2/data/stage1_ground_truth.csv'
+severity_ground_truth_path = 'vision_models/stage2/data/train.csv'
+stratification_variables_path = 'vision_models/stage2/data/train.csv'
+
+predictions_df = pd.read_csv(ground_truth_path)
+severity_df = pd.read_csv(severity_ground_truth_path)
+stratification_variables = pd.read_csv(stratification_variables_path)
+
+balance_variables = stratification_variables.columns.difference(['study_id', 'series_id'])
+
+dataset_instance = SeverityPredictionDataset(
+    disease_predictions=predictions_df,
+    balance_variables=balance_variables,
+    severity_ground_truth = severity_df
+)
+
+stage_1_target_labels = pd.read_csv('vision_models/stage2/data/train_label_coordinates.csv')
+dataset  = dataset_instance.load_data("test")
+
+# Create an iterator
+iterator = iter(dataset)
+
+try:
+    element = dataset.take(1)
+    for img, label in element:
+        print("Image tensor shape: ", img.shape)  
+        print("Label tensor shape: ", label.shape)
+        break  
+except tf.errors.OutOfRangeError:
+    print("End of dataset")
